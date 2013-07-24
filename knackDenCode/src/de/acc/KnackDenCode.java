04550e6e87ed62c16cf9663d2d25c729196c946b
Carsten <carsten@terlutter.net>
2013-07-24 15:00:11 +0200

    first Commit

---
+++ knackDenCode/src/de/acc/KnackDenCode.java
@@ -0,0 +1,70 @@
+package de.acc;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+/**
+ * @author Carsten
+ *
+ */
+public class KnackDenCode {
+	static class Masher {
+		static byte[] mash(String s){
+			System.out.println("mashing String: '" + s + "'");
+			byte[] bytes = s.getBytes();
+			byte[] mashed = new byte[bytes.length];
+			
+			for (int i = 0; i < bytes.length; i++) {
+				mashed[i] = (byte) ~bytes[i];
+			}
+			System.out.println("in:\t" + Arrays.toString(bytes));
+			System.out.println("out:\t" + Arrays.toString(mashed));
+			return mashed;
+		}
+		
+		static String unmash(byte[] s) {
+			System.out.println("unmashing....");
+			byte[] bytes = s;
+			byte[] unmashed = new byte[bytes.length];
+			
+			for (int i = 0; i < bytes.length; i++) {
+				unmashed[i] = (byte) ~bytes[i];
+			}
+			System.out.println("in:\t" + Arrays.toString(bytes));
+			System.out.println("out:\t" + Arrays.toString(unmashed));
+			System.out.println("unmashed String: '" + new String(unmashed) + "'");
+			return new String(unmashed);
+		}
+	}
+
+	public static void main(String[] args) {
+		// Test the masher
+		String testString = "Ein Team, ein Ziel, ein Weg.";
+		//testString = "Die soll ein langer und lustiger Satz sein, denn ich will testen ob es funktioniert :D";
+		
+		/***  Debugging
+		System.out.println("String:\t" + testString + " (" + testString.length() + ")");
+		
+		byte[] out1 = testString.getBytes();
+		System.out.println("oByte:\t" + Arrays.toString(out1) + " (" + out1.length + ")");
+		
+		byte[] mashed = Masher.mash(testString);
+		String unmashed = Masher.unmash(mashed);
+		
+		System.out.println("Out:\t" + unmashed + " (" + unmashed.length() + ")");
+		
+		*****/
+		
+		if (Masher.unmash(Masher.mash(testString)).equals(testString)){
+			System.out.println("OK");
+		} else {
+			System.out.println("Fehler");
+		}
+		
+
+	}
+	public static String toHex(String arg) {
+		return String.format("%040x", new BigInteger(arg.getBytes()));
+	}
+
+}
